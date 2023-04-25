7cdfb550f2fa5314d1f4c7ee758101a57c13958f
71904_Prafull <83003290+PrafulNimje@users.noreply.github.com>
2023-04-25 12:54:21 +0530

    first commit

---
+++ ArithmeticOperations.java
@@ -0,0 +1,16 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        
+        int add = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+        
+        System.out.println("Addition of " + a + " and " + b + " is " + add);
+        System.out.println("Substraction of " + a + " and " + b + " is " + sub);
+        System.out.println("Multiplication of " + a + " and " + b + " is " + mul);
+        System.out.println("Division of " + a + " and " + b + " is " + div);
+    }
+}
